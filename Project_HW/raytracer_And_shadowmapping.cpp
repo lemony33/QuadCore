897c2c9baf67c8a4bc43339d6a762d844d9b9ae8
CHOI-XH57\Hyeonu Choi <renewal1990@gmail.com>
2017-04-11 20:59:43 +0900

    raytracer 예제와 shadowmapping을 합치기 위한 생쇼

--- Project_HW/raytracer_And_shadowmapping.cpp
+++ Project_HW/raytracer_And_shadowmapping.cpp
@@ -27,12 +27,13 @@
 #include <object.h>
 #include <sb7ktx.h>
 #include <shader.h>
-#include "shadowmappingHead.h"
+
+#define DEPTH_TEXTURE_SIZE      4096
+#define FRUSTUM_DEPTH           1000
+const int NUMBER = 128;
 
 class ray_and_shadow : public sb7::application
 {
-public:
-	shadowmapping_app sm;
 public:
 	ray_and_shadow()
 		: prepare_program(0),
@@ -41,6 +42,10 @@ public:
 		max_depth(1),
 		debug_depth(0),
 		debug_mode(DEBUG_NONE),
+		light_program(0), // shadow
+		view_program(0), // shadow
+		show_light_depth_program(0), // shadow
+		mode(RENDER_FULL), // shadow
 		paused(false)
 	{
 	}
@@ -48,7 +53,7 @@ public:
 protected:
 	void init()
 	{
-		static const char title[] = "OpenGL SuperBible - Ray Tracing";
+		static const char title[] = "OpenGL SuperBible - Ray Tracing + Shadow Mapping";
 
 		sb7::application::init();
 
@@ -80,6 +85,10 @@ protected:
 	GLuint          trace_program;
 	GLuint          blit_program;
 
+	GLuint          light_program; // shadow
+	GLuint          view_program; // shadow
+	GLint           show_light_depth_program; // shadow
+
 	struct uniforms_block
 	{
 		vmath::mat4     mv_matrix;
@@ -98,6 +107,46 @@ protected:
 		GLint           ray_lookat;
 		GLint           aspect;
 	} uniforms;
+	/////////////////////////////////////////////////////////////////////
+	struct
+	{
+		struct
+		{
+			GLint   mvp;
+		} light;
+		struct
+		{
+			GLint   mv_matrix;
+			GLint   proj_matrix;
+			GLint   shadow_matrix;
+			GLint   full_shading;
+		} view;
+	} smuniforms; 
+
+	GLuint          depth_fbo;
+	GLuint          depth_tex;
+	GLuint          depth_debug_tex;
+
+	enum { OBJECT_COUNT = 4 };
+	struct
+	{
+		sb7::object     obj;
+		vmath::mat4     model_matrix;
+	} objects[OBJECT_COUNT];
+
+	vmath::mat4     light_view_matrix;
+	vmath::mat4     light_proj_matrix;
+
+	vmath::mat4     camera_view_matrix;
+	vmath::mat4     camera_proj_matrix;
+	
+	enum
+	{
+		RENDER_FULL,
+		RENDER_LIGHT,
+		RENDER_DEPTH
+	} mode;
+	////////////////////////////////////////////////////////////////// shadow
 
 	GLuint          vao;
 
@@ -170,21 +219,61 @@ void ray_and_shadow::startup()
 	int i;
 	load_shaders();
 
+	static const char * const object_names[] =
+	{
+		"../media/objects/dragon.sbm",
+		"../media/objects/sphere.sbm",
+		"../media/objects/cube.sbm",
+		"../media/objects/torus.sbm"
+	};
+
+	for (i = 0; i < OBJECT_COUNT; i++)
+	{
+		objects[i].obj.load(object_names[i]);
+	}
+
+	glGenFramebuffers(1, &depth_fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, depth_fbo);
+
+	glGenTextures(1, &depth_tex);
+	glBindTexture(GL_TEXTURE_2D, depth_tex);
+	glTexStorage2D(GL_TEXTURE_2D, 11, GL_DEPTH_COMPONENT32F, DEPTH_TEXTURE_SIZE, DEPTH_TEXTURE_SIZE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depth_tex, 0);
+
+	glGenTextures(1, &depth_debug_tex);
+	glBindTexture(GL_TEXTURE_2D, depth_debug_tex);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_R32F, DEPTH_TEXTURE_SIZE, DEPTH_TEXTURE_SIZE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, depth_debug_tex, 0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	//////////////////////////////////////////////////////////
 	glGenBuffers(1, &uniforms_buffer);
 	glBindBuffer(GL_UNIFORM_BUFFER, uniforms_buffer);
 	glBufferData(GL_UNIFORM_BUFFER, sizeof(uniforms_block), NULL, GL_DYNAMIC_DRAW);
 
 	glGenBuffers(1, &sphere_buffer);
 	glBindBuffer(GL_UNIFORM_BUFFER, sphere_buffer);
-	glBufferData(GL_UNIFORM_BUFFER, 128 * sizeof(sphere), NULL, GL_DYNAMIC_DRAW);
+	glBufferData(GL_UNIFORM_BUFFER, NUMBER * sizeof(sphere), NULL, GL_DYNAMIC_DRAW);
 
 	glGenBuffers(1, &plane_buffer);
 	glBindBuffer(GL_UNIFORM_BUFFER, plane_buffer);
-	glBufferData(GL_UNIFORM_BUFFER, 128 * sizeof(plane), NULL, GL_DYNAMIC_DRAW);
+	glBufferData(GL_UNIFORM_BUFFER, NUMBER * sizeof(plane), NULL, GL_DYNAMIC_DRAW);
 
 	glGenBuffers(1, &light_buffer);
 	glBindBuffer(GL_UNIFORM_BUFFER, light_buffer);
-	glBufferData(GL_UNIFORM_BUFFER, 128 * sizeof(sphere), NULL, GL_DYNAMIC_DRAW);
+	glBufferData(GL_UNIFORM_BUFFER, NUMBER * sizeof(sphere), NULL, GL_DYNAMIC_DRAW);
+
+	glEnable(GL_DEPTH_TEST); //sm
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -239,12 +328,10 @@ void ray_and_shadow::startup()
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	sm.startup();
 }
 
 void ray_and_shadow::render(double currentTime)
 {
-	sm.render(currentTime);
 	static const GLfloat zeros[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 	static const GLfloat gray[] = { 0.1f, 0.1f, 0.1f, 0.0f };
 	static const GLfloat ones[] = { 1.0f };
@@ -264,6 +351,7 @@ void ray_and_shadow::render(double currentTime)
 	- 10 * sinf(rotX / 400 * 3.14 / 2) * 10 * sinf(rotX / 400 * 3.14 / 2)
 	- 10 * sinf(rotY / 400 * 3.14 / 2) * 10 * sinf(rotY / 400 * 3.14 / 2)
 	);*/
+	vmath::vec3 light_position = vmath::vec3(20.0f, 20.0f, 20.0f); //
 	vmath::vec3 view_position = vmath::vec3(posX, posY, posZ);
 	vmath::vec3 lookat_point = vmath::vec3(posX + sinf(rotX / 400 * 3.14 / 2),
 		posY,//posY + 10 * sinf(rotY / 400 * 3.14 / 2),
@@ -272,7 +360,51 @@ void ray_and_shadow::render(double currentTime)
 	vmath::mat4 view_matrix = vmath::lookat(view_position,
 		lookat_point,
 		vmath::vec3(0.0f, 1.0f, 0.0f));
+	camera_view_matrix = view_matrix;
+	// perspective(fovy_시야각, aspect_방향_관점_면->화면비율, n, f)
+	camera_proj_matrix = vmath::perspective(100.0f,
+		(float)info.windowWidth / (float)info.windowHeight,
+		10.0f,
+		1000.0f);
 
+	light_proj_matrix = vmath::frustum(-1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 200.0f);
+	/*light_view_matrix = vmath::lookat(light_position,
+		vmath::vec3(0.0f), vmath::vec3(0.0f, 1.0f, 0.0f));*/
+	light_view_matrix = view_matrix;
+
+	objects[0].model_matrix = vmath::rotate((f + 30.0f) * 14.5f, 0.0f, 1.0f, 0.0f) *
+		vmath::rotate(20.0f, 1.0f, 0.0f, 0.0f) *
+		vmath::translate(0.0f, -4.0f, 0.0f);
+
+	objects[1].model_matrix = vmath::rotate((f + 30.0f) * 3.7f, 0.0f, 1.0f, 0.0f) *
+		vmath::translate(sinf((f + 30.0f) * 0.37f) * 12.0f, cosf((f + 30.0f) * 0.37f) * 12.0f, 0.0f) *
+		vmath::scale(2.0f);
+
+	objects[2].model_matrix = vmath::rotate((f + 30.0f) * 6.45f, 0.0f, 1.0f, 0.0f) *
+		vmath::translate(sinf((f + 30.0f) * 0.25f) * 10.0f, cosf((f + 30.0f) * 0.25f) * 10.0f, 0.0f) *
+		vmath::rotate((f + 30.0f) * 99.0f, 0.0f, 0.0f, 1.0f) *
+		vmath::scale(2.0f);
+
+	objects[3].model_matrix = vmath::rotate((f + 30.0f) * 5.25f, 0.0f, 1.0f, 0.0f) *
+		vmath::translate(sinf((f + 30.0f) * 0.51f) * 14.0f, cosf((f + 30.0f) * 0.51f) * 14.0f, 0.0f) *
+		vmath::rotate((f + 30.0f) * 120.3f, 0.707106f, 0.0f, 0.707106f) *
+		vmath::scale(2.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	render_scene(total_time, true);
+
+	if (mode == RENDER_DEPTH)
+	{
+		glDisable(GL_DEPTH_TEST);
+		glBindVertexArray(vao);
+		glUseProgram(show_light_depth_program);
+		glBindTexture(GL_TEXTURE_2D, depth_debug_tex);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	}
+	else
+	{
+		render_scene(total_time, false);
+	}
 
 	glBindBufferBase(GL_UNIFORM_BUFFER, 0, uniforms_buffer);
 	uniforms_block * block = (uniforms_block *)glMapBufferRange(GL_UNIFORM_BUFFER,
@@ -294,14 +426,14 @@ void ray_and_shadow::render(double currentTime)
 	glUnmapBuffer(GL_UNIFORM_BUFFER);
 
 	glBindBufferBase(GL_UNIFORM_BUFFER, 1, sphere_buffer);
-	sphere * s = (sphere *)glMapBufferRange(GL_UNIFORM_BUFFER, 0, 128 * sizeof(sphere), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+	sphere * s = (sphere *)glMapBufferRange(GL_UNIFORM_BUFFER, 0, NUMBER * sizeof(sphere), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
 
 	int i;
 
-	for (i = 0; i < 128; i++)
+	for (i = 0; i < NUMBER; i++)
 	{
 		// float f = 0.0f;
-		float fi = (float)i / 128.0f;
+		float fi = (float)i / (float)NUMBER;
 		s[i].center = vmath::vec3(sinf(fi * 123.0f + f) * 15.75f, cosf(fi * 456.0f + f) * 15.75f, (sinf(fi * 300.0f + f) * cosf(fi * 200.0f + f)) * 20.0f);
 		s[i].radius = fi * 2.3f + 3.5f;
 		float r = fi * 61.0f;
@@ -316,7 +448,7 @@ void ray_and_shadow::render(double currentTime)
 	glUnmapBuffer(GL_UNIFORM_BUFFER);
 
 	glBindBufferBase(GL_UNIFORM_BUFFER, 2, plane_buffer);
-	plane * p = (plane *)glMapBufferRange(GL_UNIFORM_BUFFER, 0, 128 * sizeof(plane), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+	plane * p = (plane *)glMapBufferRange(GL_UNIFORM_BUFFER, 0, NUMBER * sizeof(plane), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
 
 	//for (i = 0; i < 1; i++)
 	{
@@ -342,11 +474,11 @@ void ray_and_shadow::render(double currentTime)
 	glUnmapBuffer(GL_UNIFORM_BUFFER);
 
 	glBindBufferBase(GL_UNIFORM_BUFFER, 3, light_buffer);
-	light * l = (light *)glMapBufferRange(GL_UNIFORM_BUFFER, 0, 128 * sizeof(light), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+	light * l = (light *)glMapBufferRange(GL_UNIFORM_BUFFER, 0, NUMBER * sizeof(light), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
 
 	f *= 1.0f;
 
-	for (i = 0; i < 128; i++)
+	for (i = 0; i < NUMBER; i++)
 	{
 		float fi = 3.33f - (float)i; //  / 35.0f;
 		l[i].position = vmath::vec3(sinf(fi * 2.0f - f) * 15.75f,
@@ -413,6 +545,72 @@ void ray_and_shadow::render(double currentTime)
 	glBindVertexArray(vao);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	*/
+}
+
+void ray_and_shadow::render_scene(double currentTime, bool from_light)
+{
+	static const GLfloat ones[] = { 1.0f };
+	static const GLfloat zero[] = { 0.0f };
+	static const GLfloat gray[] = { 0.1f, 0.1f, 0.1f, 0.0f };
+	static const vmath::mat4 scale_bias_matrix = vmath::mat4(vmath::vec4(0.5f, 0.0f, 0.0f, 0.0f),
+		vmath::vec4(0.0f, 0.5f, 0.0f, 0.0f),
+		vmath::vec4(0.0f, 0.0f, 0.5f, 0.0f),
+		vmath::vec4(0.5f, 0.5f, 0.5f, 1.0f));
+	vmath::mat4 light_vp_matrix = light_proj_matrix * light_view_matrix;
+	vmath::mat4 shadow_sbpv_matrix = scale_bias_matrix * light_proj_matrix * light_view_matrix;
+
+	if (from_light)
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, depth_fbo);
+		glViewport(0, 0, DEPTH_TEXTURE_SIZE, DEPTH_TEXTURE_SIZE);
+		glEnable(GL_POLYGON_OFFSET_FILL);
+		glPolygonOffset(4.0f, 4.0f);
+		glUseProgram(light_program);
+		static const GLenum buffs[] = { GL_COLOR_ATTACHMENT0 };
+		glDrawBuffers(1, buffs);
+		glClearBufferfv(GL_COLOR, 0, zero);
+	}
+	else
+	{
+		glViewport(0, 0, info.windowWidth, info.windowHeight);
+		glClearBufferfv(GL_COLOR, 0, gray);
+		glUseProgram(view_program);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, depth_tex);
+		glUniformMatrix4fv(smuniforms.view.proj_matrix, 1, GL_FALSE, camera_proj_matrix);
+		//glUniformMatrix4fv(uniforms.ray_lookat, 1, GL_FALSE, camera_proj_matrix);
+		glDrawBuffer(GL_BACK);
+	}
+
+	glClearBufferfv(GL_DEPTH, 0, ones);
+
+	int i;
+	for (i = 0; i < 4; i++)
+	{
+		vmath::mat4& model_matrix = objects[i].model_matrix;
+		if (from_light)
+		{
+			glUniformMatrix4fv(smuniforms.light.mvp, 1, GL_FALSE, light_vp_matrix * objects[i].model_matrix);
+		}
+		else
+		{
+			vmath::mat4 shadow_matrix = shadow_sbpv_matrix * model_matrix;
+			glUniformMatrix4fv(smuniforms.view.shadow_matrix, 1, GL_FALSE, shadow_matrix);
+			glUniformMatrix4fv(smuniforms.view.mv_matrix, 1, GL_FALSE, camera_view_matrix * objects[i].model_matrix);
+			glUniform1i(smuniforms.view.full_shading, mode == RENDER_FULL ? 1 : 0);
+		}
+		objects[i].obj.render();
+	}
+
+	if (from_light)
+	{
+		glDisable(GL_POLYGON_OFFSET_FILL);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+	else
+	{
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
 }
 
 void ray_and_shadow::recurse(int depth)
@@ -489,6 +687,15 @@ void ray_and_shadow::onKey(int key, int action)
 			max_depth--;
 			if (max_depth < 1)
 				max_depth = 1;
+			break;
+		case '1':
+			mode = RENDER_FULL;
+			break;
+		case '2':
+			mode = RENDER_LIGHT;
+			break;
+		case '3':
+			mode = RENDER_DEPTH;
 			break;
 		case 'P':
 			paused = !paused;
@@ -654,6 +861,59 @@ void ray_and_shadow::load_shaders()
 		glDeleteProgram(blit_program);
 
 	blit_program = sb7::program::link_from_shaders(shaders, 2, true);
+
+	GLuint vs;
+	GLuint fs;
+
+	vs = sb7::shader::load("../media/shaders/shadowmapping/shadowmapping-light.vs.glsl", GL_VERTEX_SHADER);
+	fs = sb7::shader::load("../media/shaders/shadowmapping/shadowmapping-light.fs.glsl", GL_FRAGMENT_SHADER);
+
+	if (light_program)
+		glDeleteProgram(light_program);
+
+	light_program = glCreateProgram();
+	glAttachShader(light_program, vs);
+	glAttachShader(light_program, fs);
+	glLinkProgram(light_program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	smuniforms.light.mvp = glGetUniformLocation(light_program, "mvp");
+
+	vs = sb7::shader::load("../media/shaders/shadowmapping/shadowmapping-camera.vs.glsl", GL_VERTEX_SHADER);
+	fs = sb7::shader::load("../media/shaders/shadowmapping/shadowmapping-camera.fs.glsl", GL_FRAGMENT_SHADER);
+
+	if (light_program)
+		glDeleteProgram(view_program);
+
+	view_program = glCreateProgram();
+	glAttachShader(view_program, vs);
+	glAttachShader(view_program, fs);
+	glLinkProgram(view_program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	smuniforms.view.proj_matrix = glGetUniformLocation(view_program, "proj_matrix");
+	smuniforms.view.mv_matrix = glGetUniformLocation(view_program, "mv_matrix");
+	smuniforms.view.shadow_matrix = glGetUniformLocation(view_program, "shadow_matrix");
+	smuniforms.view.full_shading = glGetUniformLocation(view_program, "full_shading");
+
+	if (show_light_depth_program)
+		glDeleteProgram(show_light_depth_program);
+
+	show_light_depth_program = glCreateProgram();
+
+	vs = sb7::shader::load("../media/shaders/shadowmapping/shadowmapping-light-view.vs.glsl", GL_VERTEX_SHADER);
+	fs = sb7::shader::load("../media/shaders/shadowmapping/shadowmapping-light-view.fs.glsl", GL_FRAGMENT_SHADER);
+
+	glAttachShader(show_light_depth_program, vs);
+	glAttachShader(show_light_depth_program, fs);
+	glLinkProgram(show_light_depth_program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
 }
 
 DECLARE_MAIN(ray_and_shadow)
