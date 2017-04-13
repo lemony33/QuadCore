f0cbcce0d7aa31ca968c8dc0f93ca5e4b28328d0
YoungEun Lee <YoungEun Lee@DESKTOP-MUP3BFT>
2017-04-13 20:25:42 +0900

    Sonar Lighting 추가_0413

--- Project_YE/res/basicShader.fs
+++ Project_YE/res/basicShader.fs
@@ -1,16 +1,17 @@
 #version 430 core
 
-varying vec2 texCoord0;  //texture
-varying vec3 normal0;    //lighting
+varying vec2 texCoord0;  // texture
+varying vec3 normal0;    // lighting
 
-uniform sampler2D diffuse;
+uniform sampler2D diffuse;	// texture / uniform access by CPU wt and GPU r
 
 void main()
 {
 	gl_FragColor = texture2D(diffuse, texCoord0)
-		* clamp(dot(-vec3(0,0,1), normal0), 0.0, 1.0);	//lighting
+	* clamp(dot(-vec3(0,0,1), normal0), 0.0, 1.0);	//lighting
 
 	//gl_FragColor = texture2D(diffuse, texCoord0) //Texture
-	//gl_FragColor = texture2D(diffuse, vec2(0.3, 0.8)); //Texture
-	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);	//Color
+	//gl_FragColor = texture2D(diffuse, vec2(0.3, 0.8)); // 2. Texture
+
+	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);	// 1. shader 첫번째 : color 선언만 해준거임.
 }
