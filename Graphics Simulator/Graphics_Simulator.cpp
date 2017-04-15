00434af90161f16b79599cec9364fd257c71f535
lemony <lemony37@outlook.com>
2017-04-15 17:47:22 +0900

    카메라 시점 수정 중 - 중간저장

--- Graphics Simulator/Graphics_Simulator.cpp
+++ Graphics Simulator/Graphics_Simulator.cpp
@@ -4,6 +4,7 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "Controls.h"
 
 
 QuadCore::Graphics_Simulator::Graphics_Simulator()
@@ -48,7 +49,14 @@ void QuadCore::Graphics_Simulator::Run()
 	Transform transform;
 	float counter = 0.0f;
 	//5. Camera
+
+	int width = width_window;	//**
+	int height = height_window;	//**
+	display.GetFrameBufferSize(&width, &height);	//**
+	//Camera camera(glm::vec3(0, 0, 5), 70.0f, (float)width / (float)height, 0.01F, 1000.0f);
 	Camera camera(glm::vec3(0, 0, 5), 70.0f, (float)width_window / (float)height_window, 0.01F, 1000.0f);
+
+	Controls controls(display.GetWindow());
 
 	while (!display.IsClosed())
 	{
@@ -73,14 +81,15 @@ void QuadCore::Graphics_Simulator::Run()
 		shader.Update(transform, camera);
 		mesh1.Draw();
 
-		//shader2.Bind();
-		//texture2.Bind(0);
-		////transform.GetPos().x += 1.5;
-		//transform.GetPos().y += 0.5;
-		////transform.GetPos().z += 1.5;
+		shader2.Bind();
+		texture2.Bind(0);
+		//transform.GetPos().x += 1.5;
+		transform.GetPos().x -= 1.5;
+		//transform.GetPos().z += 1.5;
 		//transform.SetScale(glm::vec3(3,0.5,1));
-		//shader.Update(transform, camera);		
-		//mesh2.Draw();
+		transform.SetScale(glm::vec3(1, 1, 1));
+		shader.Update(transform, camera);		
+		mesh2.Draw();
 
 
 		//shader.Bind();
@@ -95,126 +104,8 @@ void QuadCore::Graphics_Simulator::Run()
 		shader.Update(transform, camera);
 
 		
-		//GLUquadric *sphere; // object 선언
-		//sphere = gluNewQuadric(); // object 생성
-		//gluSphere(sphere, 0.3f, 50, 10);  // sphere 그리기
-		
-		GLUquadric *cylinder;
-		cylinder = gluNewQuadric();
-		gluCylinder(cylinder, 0.8f, 0.3f, 1, 50, 10);
-		
-		
-
-		// opengl - 1버전 방식
-		//glColor3f(1.0f, 0.0f, 0.0f);
-		glBegin(GL_LINES);
-			glVertex3f(-5, 3, 0);			
-			glVertex3f(+5, 3, 0);
-		glEnd();
-
-		transform.GetPos().x = 0;
-		shader.Update(transform, camera);
-		glLineWidth(10.0);
-		glColor3f(1.f, 0.f, 0.f);		// 빨간색 지정
-		glBegin(GL_LINE_LOOP);			// x축 그리기
-		glVertex3f(1.0, 0.0, 0.0);		// 시작 점
-		glVertex3f(-1.0, 0.0, 0.0);		// 끝 점
-		glEnd();
-
-		glColor3f(0.f, 1.f, 0.f);		// 초록색 지정
-		glBegin(GL_LINE_LOOP);			// y축 그리기
-		glVertex3f(0.0, 1.0, 0.0);
-		glVertex3f(0.0, -1.0, 0.0);
-		glEnd();
-		glColor3f(0.f, 0.f, 1.f);		// 파란색 지정
-		glBegin(GL_LINE_LOOP);			// z축 그리기
-		glVertex3f(0.0, 0.0, 1.0);
-		glVertex3f(0.0, 0.0, -1.0);
-		glEnd();
-
-		//GLfloat vertices[][3] = {
-		//							{ -1.0,-1.0,-1.0 },	// 0
-		//							{ 1.0,-1.0,-1.0 },	// 1
-		//							{ 1.0,1.0,-1.0 },	// 2
-		//							{ -1.0,1.0,-1.0 },	// 3
-		//							{ -1.0,-1.0,1.0 },
-		//							{ 1.0,-1.0,1.0 },
-		//							{ 1.0,1.0,1.0 },
-		//							{ -1.0,1.0,1.0 }
-		//						};
-		//
-		//glBegin(GL_QUADS);
-		//glVertex3fv(vertices[0]);
-		//glVertex3fv(vertices[1]);
-		//glVertex3fv(vertices[2]);
-		//glVertex3fv(vertices[3]);
-		//glEnd();
-
-		//// opengl - 2버전 방식
-			
-
-		//float points[] = {
-		//	-10, 10,
-		//	-10, 5,
-		//	-5, 5,
-		//	-5, 10
-		//};
-		//glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, points);
-		//glEnableVertexAttribArray(0);
-		//glDrawArrays(GL_QUADS, 0, 4);
-
-		float quadVertices[] = {
-			-1.0,-1.0, 0.0, 
-			1.0,-1.0, 0.0, 
-			1.0,1.0, 0.0,
-			-1.0,1.0, 0.0,
-		};
-
-		//glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, quadVertices);		
-		//glEnableVertexAttribArray(0);
-
-		//glEnableVertexAttribArray(0);
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		//glVertexPointer(3, GL_FLOAT, 0, quadVertices);
-		//glDrawArrays(GL_QUADS, 0, 4);
-
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glEnableVertexAttribArray(0);
-		
 
 		display.Update();
 		counter += 0.05f;
 	}
 }
-
-/*
-GitHub Wiki 예시
-https://github.com/antaehyeon/Hyeon-Schedule/wiki
-
-OpenGL 보조 라이브러리 종류
-GL/GLES/EGL/GLX/WGL
-
-
-OpenGL - 1.0 버전 그리기
-glBegin( GL_QUADS )
-glVertex2f(  10, 10 );
-glVertex2f(  10, -10 );
-glVertex2f( -10, -10 );
-glVertex2f( -10, 10 );
-glEnd()
-
-OpenGL - 2.0 버전 그리기
-float points[] = {
-10, 10,
-10, -10,
--10, -10,
--10, 10
-};
-glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, points);
-glEnableVertexAttribArray(0);
-glDrawArrays( GL_QUADS, 0, 4 );
-*/
