6aa3a500671196faf0a7f9304a91093a782d13b8
lemony <lemony37@outlook.com>
2017-05-06 16:27:41 +0900

    미니좌표계, 월드 좌표계, 그리드맵 구현위치 이동, 오브젝트화, On/Off 기능 추가

---
+++ Graphics Simulator/miniUI_coordinate.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "iShape.h"
+
+namespace QuadCore
+{
+
+class miniUI_coordinate : public iShape
+{
+public:
+
+	miniUI_coordinate()
+	{
+		m_transform = new Transform();
+		m_coordinate = new Coordinates(1.0f);
+		
+		m_shader = new Shader("../media/new_shader/basicShader_2D_HUD");
+		m_texture = new Texture("../media/res/bricks.jpg");
+	}
+
+	virtual ~miniUI_coordinate()
+	{
+	}
+
+public:
+	virtual void Draw(QuadCore::Camera* camera, bool enable_coor)
+	{
+		m_shader->Bind();
+		m_texture->Bind(0);
+
+		m_transform->GetPos().x = +0.7f;
+		m_transform->GetPos().y = -0.7f;
+
+		m_transform->SetRot(camera->GetRot());
+
+		m_shader->Update(*m_transform, *camera);
+
+		if (enable_coor)
+		{
+			m_coordinate->Init_UI(m_transform, camera, 0.1f);
+			m_coordinate->Draw(5.0f);
+		}
+	}
+};
+
+}
