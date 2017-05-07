eb48002755f875cc15730abb10c66fbd4c53f272
lemony <lemony37@outlook.com>
2017-05-07 23:34:56 +0900

    프로그램 실행시 전체화면 활성화 (테스트 용도로 비활성 놓았음, Display 4줄만 주석처리하면 비활성화됨)

--- Graphics Simulator/Display.cpp
+++ Graphics Simulator/Display.cpp
@@ -10,11 +10,11 @@ Display::Display(int width, int height, const std::string& title)
 		return;
 		//return -1;
 
-	//###
-	//monitor = glfwGetPrimaryMonitor();
-	//mode = glfwGetVideoMode(monitor);
-	//width = mode->width;
-	//height = mode->height;
+	//### 프로그램 실행시 전체화면 활성화하기
+	monitor = glfwGetPrimaryMonitor();
+	mode = glfwGetVideoMode(monitor);
+	width = mode->width;
+	height = mode->height;
 	//###
 
 	/* Create a windowed mode window and its OpenGL context */
